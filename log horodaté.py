13cfc05b7b2cea016d88d5c4733896f3b3121482
sarrabejaoui923@gmail.com <sarrabejaoui923@gmail.com>
2022-03-19 13:50:09 +0100

    premier commit

---
+++ log horodaté.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger("monlog")
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler("monlog.log")
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.info("mon message info")
+
